1f97e6603b14c4fcde5bc6c3433b53e8b4ac6855
Joanne Huang <chwong0314@me.com>
2024-04-14 18:17:19 -0400

    Add player manager script that can update the player UI

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
-using UnityEngine.UIElements;
-
+using TMPro;
+using UnityEngine.UI;
+using Unity.VisualScripting;
 
 public enum playerCall
 {
@@ -11,18 +12,40 @@ public enum playerCall
     Collab,
     NotCollab
 }
+
+public enum playerStatus
+{
+    Selecting,
+    Ready,
+    Action,
+    Hold,
+}
+
 public class PlayerManager : MonoBehaviour
 {
     public int playerNo;
 
     public int playerCoin;
     public playerCall playerCall;
+    public playerStatus playerStatus;
+
+    //UI
+    public TextMeshProUGUI playerIDtext;
+    public TextMeshProUGUI playerCoinText;
+    public TextMeshProUGUI playerStatusText;
+    public GameObject playerStatusSignSymbol;
+
     
     // Start is called before the first frame update
     void Start()
     {
         playerCoin = 0;
         playerCall = playerCall.None;
+
+        playerIDtext.text = "Player " + playerNo.ToString();
+        playerCoinText.text = playerCoin.ToString();        
+        SetPlayerStatus(playerStatus.Selecting);
+        playerStatusSignSymbol.GetComponent<UpdateStatusSignSymbol>().SetStatusSignSymbolImg(playerStatus);
     }
 
     // Update is called once per frame
@@ -49,6 +72,36 @@ public class PlayerManager : MonoBehaviour
     public int GetPlayerCoin()
     {
         return playerCoin;
+    }
+
+    public void SetPlayerStatus(playerStatus state)
+    {
+        playerStatus = state;
+        
+        switch (state)
+        {
+            case playerStatus.Selecting:
+                playerStatusText.text = "Selecting";
+                // Debug.Log("Player " + playerNo + " is selecting");
+                break;
+            case playerStatus.Ready:
+                playerStatusText.text = "Ready";
+                // Debug.Log("Player " + playerNo + " is ready");
+                break;
+            case playerStatus.Action:
+                playerStatusText.text = "Action"; 
+                // Debug.Log("Player " + playerNo + " is action");
+                break;
+            case playerStatus.Hold:
+                playerStatusText.text = "Hold";
+                // Debug.Log("Player " + playerNo + " is hold");
+                break;
+        }
+    }
+
+    public playerStatus GetPlayerState()
+    {
+        return playerStatus;
     }
 
 
---
+++ Assets/Scripts/UpdateStatusSignSymbol.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateStatusSignSymbol : MonoBehaviour
+{
+    //status images
+    public Sprite questionImg;
+    public Sprite checkImg;
+    
+    public GameObject playerManager;
+    Image StatusSignSymbolImg;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StatusSignSymbolImg = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetStatusSignSymbolImg (playerStatus state)
+    {
+        switch (state)
+        {
+            case playerStatus.Selecting:
+                StatusSignSymbolImg.sprite = questionImg;
+                break;
+            case playerStatus.Ready:
+                StatusSignSymbolImg.sprite = checkImg;
+                break;
+            case playerStatus.Action:
+                StatusSignSymbolImg.sprite = null;
+                break;
+            case playerStatus.Hold:
+                StatusSignSymbolImg.sprite = null;
+                break;
+            default:
+                Debug.Log("fail to set StatusSignSymbolImg");
+                break;
+        }
+    }
+}
