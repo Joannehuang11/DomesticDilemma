4d5745b3a6369043ba6543ad80feff2f7b72dca9
Joanne Huang <chwong0314@me.com>
2024-04-14 16:22:35 -0400

    Add player manager

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+using UnityEngine.UIElements;
 
+
+public enum playerCall
+{
+    None,
+    Collab,
+    NotCollab
+}
 public class PlayerManager : MonoBehaviour
 {
+    public int playerNo;
+
+    public int playerCoin;
+    public playerCall playerCall;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerCoin = 0;
+        playerCall = playerCall.None;
     }
 
     // Update is called once per frame
@@ -15,4 +30,25 @@ public class PlayerManager : MonoBehaviour
     {
         
     }
+
+    public void SetPlayerCall(playerCall call)
+    {
+        playerCall = call;
+    }
+
+    public playerCall GetPlayerCall()
+    {
+        return playerCall;
+    }
+
+    public void SetPlayerCoin(int coin)
+    {
+        playerCoin = coin;
+    }
+    public int GetPlayerCoin()
+    {
+        return playerCoin;
+    }
+
+
 }
